0b1e8a713679d368b57aededa45e2e6fa3a14e40
Kiarash Bahmani <kbahmani@ripple.com>
2018-01-29 14:19:39 -0800

    Added react-router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
 import logo from '../../common/statics/image/logo.png'
 import './style.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import About from '../About'
 import Contact from '../Contact'
+import Home from '../Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          UI React base
+      <Router>
+        <div className="app">
+          <div className="app-header">
+            <img src={logo} className="app-logo" alt="logo" />
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">contact</Link>
+            </div>
+          </div>
+          <div className="app-body">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+            </Switch>
+          </div>
         </div>
-        <div className="app-body">
-          <About />
-          <hr />
-          <Contact />
-        </div>
-      </div>
+      </Router>
     )
   }
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <span>This is Home component</span>
+      </div>
+    )
+  }
+}
+
+export default Home
